e912c4d4dd911572aaeb32b247f5a95381003734
filippnis <fil.nis61@gmail.com>
2023-04-07 10:30:57 +0300

    Поправил отступ по правилу PEP8

--- test_1.py
+++ test_1.py
@@ -2,14 +2,17 @@ from selene.support.shared import browser
 from selene import be, have
 import pytest
 
+
 @pytest.fixture
 def browser_open():
     browser.open('https://google.com')
     browser.driver.set_window_size(1920, 1080)
 
+
 def test_result(browser_open):
     browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
     browser.element('[id="search"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
+
 
 def test_no_result(browser_open):
     browser.element('[name="q"]').should(be.blank).type('ertfghvbnmjhvby4%$vfx crf hgfc').press_enter()
